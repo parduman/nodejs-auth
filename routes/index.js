a56b4fadc4f98663867a628ac75fbf265e18b91b
parduman <pardumangarg7@gmail.com>
2022-01-15 14:54:13 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/checkAuth')
+const path = require('path');
 
 //------------ Welcome Route ------------//
 router.get('/', (req, res) => {
@@ -8,8 +9,11 @@ router.get('/', (req, res) => {
 });
 
 //------------ Dashboard Route ------------//
-router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dash', {
-    name: req.user.name
-}));
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dash', {
+        name: req.user.name
+    })
+    // res.redirect('http://127.0.0.1:5500/codeBook.html')
+});
 
 module.exports = router;
